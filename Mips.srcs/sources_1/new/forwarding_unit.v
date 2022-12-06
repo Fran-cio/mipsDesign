3bd22944a14a28f9d8786fddea127d9e53a4eae5
Juan Manuel Gil <juanmanuelgil99@gmail.com>
2022-12-06 19:19:19 -0300

    feat: implementación de la forwarding_unit

---
+++ Mips.srcs/sources_1/new/forwarding_unit.v
@@ -0,0 +1,85 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 06.12.2022 10:52:48
+// Design Name: 
+// Module Name: mask_a_byte NO TE OLVIDES DE PONER BIEN EL NOMBRE, COPYPASTERO <-----------------------------------------------
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module forwarding_unit(
+    input   [6-1 : 0]       i_rs_if_id,
+    input   [6-1 : 0]       i_rt_if_id,
+    input   [6-1 : 0]       i_rd_id_ex,
+    input   [6-1 : 0]       i_rd_ex_mem,
+    input   [6-1 : 0]       i_rd_mem_wb,
+    input                   i_reg_wr_ex_mem,
+    input                   i_reg_wr_id_ex,
+    input                   i_reg_wr_mem_wb,
+    output  [2-1 : 0]       o_forward_a,
+    output  [2-1 : 0]       o_forward_b
+);
+    assign o_forward_a = (i_reg_wr_id_ex && (i_rd_id_ex != 0) && (i_rd_id_ex == i_rs_if_id)) 
+        ? 2'b11 //Lo saca de la ALU (etapa de id/ex)
+        : (i_reg_wr_ex_mem && (i_rd_ex_mem != 0) && (i_rd_ex_mem == i_rs_if_id)) 
+        ? 2'b01 //Lo saca de la salida de la ALU (etapa ex/mem)
+        : (i_reg_wr_mem_wb && (i_rd_mem_wb != 0) && (i_rd_mem_wb == i_rs_if_id)) 
+        ? 2'b10 //Lo saca del WB
+        : 2'b00; //Lo saca del registro
+
+    assign o_forward_b = (i_reg_wr_id_ex && (i_rd_ex_mem != 0) && (i_rd_id_ex == i_rt_if_id)) 
+        ? 2'b11 //Lo saca de la ALU (etapa id/ex)
+        : (i_reg_wr_ex_mem && (i_reg_wr_ex_mem != 0) && (i_rd_ex_mem == i_rt_if_id))
+        ? 2'b01 //Lo saca de la ALU /etapa ex/mem
+        : (i_reg_wr_mem_wb && (i_rd_mem_wb != 0) && (i_rd_mem_wb == i_rt_if_id))
+        ? 2'b10 //Lo saca del WB
+        : 2'b00; //Lo saca del registro
+
+/*
+    always @(*)  //Forward A
+    begin
+        if (i_reg_wr_id_ex 
+        && (i_rd_id_ex != 0)
+        && (i_rd_id_ex == i_rs_if_id)) 
+            forward_data_a = 2'b11; //Lo saca de la ALU (etapa de id/ex)
+        else if (i_reg_wr_ex_mem 
+        && (i_rd_ex_mem != 0)
+        && (i_rd_ex_mem == i_rs_if_id)) 
+            forward_data_a = 2'b01; //Lo saca de la salida de la ALU (etapa ex/mem)
+        else if (i_reg_wr_mem_wb 
+        && (i_rd_mem_wb != 0)
+        && (i_rd_mem_wb == i_rs_if_id))
+            forward_data_a = 2'b10; //Lo saca del WB
+    end
+*/
+/*
+    always @(*)  //Forward B
+    begin
+        if (i_reg_wr_id_ex
+        && (i_rd_ex_mem != 0)
+        && (i_rd_id_ex == i_rt_if_id))
+            forward_data_b = 2'b11; //Lo saca de la ALU (etapa id/ex)
+        else if (i_reg_wr_ex_mem
+        && (i_reg_wr_ex_mem != 0)
+        && (i_rd_ex_mem == i_rt_if_id))
+            forward_data_b = 2'b01; //Lo saca de la ALU /etapa ex/mem
+        else if (i_reg_wr_mem_wb
+        && (i_rd_mem_wb != 0)
+        && (i_rd_mem_wb == i_rt_if_id))
+            forward_data_b = 2'b10;
+    end
+*/
+endmodule
