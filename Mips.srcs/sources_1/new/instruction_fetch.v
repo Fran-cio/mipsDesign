b57cd63d52547bdbd94678540ea44d0fe47c5b77
Francisco Ciordia <gatupancho@gmail.com>
2022-12-11 16:21:58 -0300

    feat: Agregado el enable al latch

--- Mips.srcs/sources_1/new/instruction_fetch.v
+++ Mips.srcs/sources_1/new/instruction_fetch.v
@@ -28,7 +28,8 @@ module instruction_fetch(
     input                   i_word_is_ready,
     input   [32 - 1 : 0]    i_word_sended, //la word que nos envía la suod a través del formador de palabras
     output  [32 - 1 : 0]    o_instruction,
-    output                  o_is_end //cuando se termina de cargar la memoria de inst
+    output                  o_is_end, //cuando se termina de cargar la memoria de inst
+    output  [32 - 1 : 0]    o_pc_value
 );
 
 pc_unit pc_unit(
--- Mips.srcs/sources_1/new/pc_unit.v
+++ Mips.srcs/sources_1/new/pc_unit.v
@@ -19,27 +19,29 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module pc_unit(
+module pc_unit#(
+    parameter   instruccion_TAM = 32
+    )(
     input                   i_clk,
     input                   i_reset,
+    
     input                   i_pc_reset,
     input                   i_stall,
     input   [32 - 1 : 0]    i_new_pc,
     output  [32 - 1 : 0]    o_pc_value
 );
 
-reg [32 - 1 : 0] data_pc;
-reg [32 - 1 : 0] pc_value_reg;
+reg [instruccion_TAM - 1 : 0] pc_value_reg;
 
 always @(posedge i_clk)
 begin
-    data_pc <= (i_pc_reset || i_reset) ? 0 : i_new_pc;
+    if(i_reset  ||  i_pc_reset)
+        pc_value_reg     <=  0;
+    else
+        if(i_stall)
+            pc_value_reg <=  i_new_pc;
 end
 
-always @(negedge i_clk)
-begin
-    pc_value_reg <= i_stall ? pc_value_reg : data_pc;
-end
 
 assign o_pc_value = pc_value_reg;
 
